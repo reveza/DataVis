3b25e153b5aa5cce32550af347b53864481d82c6
Nicolas Normand <nicolas-2.normand@polymtl.ca>
2020-06-21 18:32:41 -0400

    version de la carte adaptée pour montréal, québec et canada

--- Map/assets/scripts/2-map.js
+++ Map/assets/scripts/2-map.js
@@ -1,16 +1,12 @@
 "use strict";
 
-/**
- * @see https://gist.github.com/d3noob/9211665
- */
-
 function initMap(L, map) {
   L.tileLayer(
     'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
       maxZoom: 12,
       minZoom: 2
   }).addTo(map);
-  map.setView([62, -97], 4);
+  map.setView([63, -97], 4);
 
   let svg = d3.select(map.getPanes().overlayPane).append("svg");
   svg.append("g").attr("class", "leaflet-zoom-hide");
@@ -18,47 +14,42 @@ function initMap(L, map) {
   return svg;
 }
 
-function createMapBorders(g, path, canada, showPanel) {
+function createMapBorders(g, path, borders) {
   g.selectAll('path')
-    .data(canada.features)
+    .data(borders.features)
     .enter()
       .append('path')
       .attr('d', path)
       .attr('class', 'district')
-      .style('fill', '#99ccff')
-      .on('click', function(d) {
-        d3.selectAll(".district").classed("selected",false);
-        d3.select(this).classed("selected",true);
-        showPanel(d.properties.NUMCF);
-      });
+      .style('fill', '#C0E2FA')
+      
 }
 
-function createMapCircles(g, canada, sources, circles) {
+function createMapCircles(g, borders, sources, path, abbreviations, date, tip, region) {
+  console.log(sources)
   g.selectAll('circle')
-    .data(canada.features)
+    .data(borders.features)
     .enter()
       .append('circle')
-      .attr('r', 10)
+      .attr('r', function(d) {
+        var zone;
+        if(region == "montreal")
+          zone = abbreviations.find(zone => zone['name'] == d.properties['district']).abbreviation;
+        else
+          zone = abbreviations.find(zone => zone['name'] == d.properties['name']).abbreviation;
+        
+        return Math.sqrt(sources[date].find(variable => variable['name'] == zone)['percentage'])*200;
+      })
       .attr('class', 'district')
-      .attr('cx', function(d) {return circles.centroid(d)[0];})
-      .attr('cy', function(d) {return circles.centroid(d)[1];})
-      .style('fill', '#C52A0D');
+      .attr('cx', function(d) {return path.centroid(d)[0];})
+      .attr('cy', function(d) {return path.centroid(d)[1];})
+      .style('fill', '#C52A0D')
+      .on("mouseover", tip.show)
+      .on("mouseout", tip.hide);
 }
 
-/**
- * Upate the position and the size of the SVG element, the position of the group "g" and the display of the traces
- * when the position or the zoom of the map is modified. 
- *
- * @param svg       The SVG element used to trace the elements over the Leaflet map. 
- * @param g         The group where the traces of the districts are created. 
- * @param path      The function that should be used to trace the geometric entities according to 
- *                  the correct projection. 
- * @param canada    The geographic entities that should be used to trace the districts. 
- *
- * @see https://gist.github.com/d3noob/9211665
- */
-function updateMap(svg, g, path, canada, circles) {
-  let bounds = path.bounds(canada);
+function updateMap(svg, g, path, borders) {
+  let bounds = path.bounds(borders);
   
   let leftBound = bounds[0][0];
   let rightBound = bounds[1][0];
@@ -71,8 +62,8 @@ function updateMap(svg, g, path, canada, circles) {
   .style("top", topBound + "px");
   
   g.selectAll('circle')
-    .attr('cx', function(d) {return circles.centroid(d)[0];})
-    .attr('cy', function(d) {return circles.centroid(d)[1];})
+    .attr('cx', function(d) {return path.centroid(d)[0];})
+    .attr('cy', function(d) {return path.centroid(d)[1];})
 
   g.attr("transform", "translate(" + -leftBound + "," + -topBound + ")");
 
